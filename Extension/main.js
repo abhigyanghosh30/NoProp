0b82100adc78290b4e569c2d9e23a66e0981c16c
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-01-16 14:51:20 +0530

    the api works now

--- Extension/main.js
+++ Extension/main.js
@@ -1,29 +1,34 @@
-document.body.style.border = "5px solid red";
+// document.body.style.border = "5px solid red";
 var spans = document.getElementsByTagName("span");
 for (var i = 0; i < spans.length; i++) {
-  if (spans[i].innerHTML.includes("propaganda")) {
-    spans[i].style.color = "red";
-    spans[i].style.backgroundColor = "yellow";
-  }
+  checkPara(spans[i].innerHTML, "s", i);
 }
 var paragraphs = document.getElementsByTagName("p");
 for (let i = 0; i < paragraphs.length; i++) {
-  checkPara(paragraphs[i].innerHTML);
+  checkPara(paragraphs[i].innerHTML, "p", i);
 }
 
-function checkPara(data = "") {
+function checkPara(sentence = "", type = "p", index = 0) {
   fetch("http://localhost:5000", {
     method: "POST",
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(sentence),
   })
     .then((res) => {
-      res.json();
+      return res.json();
     })
-    .then((res) => {
-      console.log(res);
+    .then((data) => {
+      if (data["bool"] == "1") {
+        if (type == "p") {
+          paragraphs[index].style.color = "red";
+          paragraphs[index].style.backgroundColor = "yellow";
+        } else if (type == "s") {
+          spans[index].style.color = "red";
+          spans[index].style.backgroundColor = "yellow";
+        }
+      }
     });
 }
