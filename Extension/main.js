05e79390496b1378c3b6f18b365da4f30bf5cc5b
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-03-10 22:30:43 +0530

    Changed extension to look for article all at once

--- Extension/main.js
+++ Extension/main.js
@@ -1,34 +1,29 @@
 // document.body.style.border = "5px solid red";
-var spans = document.getElementsByTagName("span");
-for (var i = 0; i < spans.length; i++) {
-  checkPara(spans[i].innerHTML, "s", i);
-}
+// var spans = document.getElementsByTagName("span");
+// for (var i = 0; i < spans.length; i++) {
+//   checkPara(spans[i].innerHTML, "s", i);
+// }
 var paragraphs = document.getElementsByTagName("p");
+let full_article = "";
 for (let i = 0; i < paragraphs.length; i++) {
-  checkPara(paragraphs[i].innerHTML, "p", i);
+  full_article += paragraphs[i].innerText;
 }
 
-function checkPara(sentence = "", type = "p", index = 0) {
-  fetch("http://localhost:5000", {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(sentence),
+fetch("http://localhost:5000", {
+  method: "POST",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(full_article),
+})
+  .then((res) => {
+    return res.json();
   })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      if (data["bool"] == "1") {
-        if (type == "p") {
-          paragraphs[index].style.color = "red";
-          paragraphs[index].style.backgroundColor = "yellow";
-        } else if (type == "s") {
-          spans[index].style.color = "red";
-          spans[index].style.backgroundColor = "yellow";
-        }
-      }
-    });
-}
+  .then((data) => {
+    if (data["bool"] == "1") {
+      window.alert(
+        "This page may contain malicious content. Use your discretion"
+      );
+    }
+  });
