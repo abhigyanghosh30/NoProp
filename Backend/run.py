099205a9a9d75234c9d0beaae93dabefa40de158
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-04-15 00:35:57 +0530

    Fancified extension for fanciful eyes

--- Backend/run.py
+++ Backend/run.py
@@ -4,28 +4,46 @@ import pandas as pd
 
 modelpath_liar = '/home/masterg/Documents/8th Semester/Social Computing/NoProp/Model/liar_BERT'
 modelpath_prop = '/home/masterg/Documents/8th Semester/Social Computing/NoProp/Model/proppy_BERT'
-model_liar = Classifier.load(modelpath_liar)
-model_prop = Classifier.load(modelpath_prop)
-df = pd.read_csv('labels.csv')
+
+model_liar = None
+model_prop = None
+df = None
 
 app = FastAPI()
 
 
-@app.get('/sources')
-async def sources(request: Request):
-    data = await request.json()
-    try:
-        res = df[df['source_url'] == data['source']].to_dict('records')[0]
-        res['valid'] = True
-        return res
-    except:
-        return {"valid": False}
+@app.on_event('startup')
+async def startup_event():
+    global model_liar
+    model_liar = Classifier.load(modelpath_liar)
+    global model_prop
+    model_prop = Classifier.load(modelpath_prop)
+    global df
+    df = pd.read_csv('labels.csv')
+
+
+def normalize_url(source):
+    source = source.replace("http://", "")
+    source = source.replace("https://", "")
+    source = source.replace("www.", "")
+    source = source.replace("/", "")
+    return source
 
 
 @app.post('/')
 async def hello_world(request: Request):
     data = await request.json()
-    predictions_prop = model_prop.predict(data['article'])
-    predictions_liar = model_liar.predict(data['article'])
-    print(predictions_liar, predictions_prop)
-    return {"prop": str(predictions_prop[0]), "liar": str(predictions_liar[0])}
+    res = {}
+    try:
+        res = df[df['source_url_normalized'] == normalize_url(
+            data['source'])].to_dict('records')[0]
+        res['valid'] = True
+        predictions_prop = model_prop.predict(data['article'])
+        predictions_liar = model_liar.predict(data['article'])
+        print(predictions_liar, predictions_prop)
+        res["prop"] = str(predictions_prop[0])
+        res["liar"] = str(predictions_liar[0])
+    except IndexError:
+        print("source not in list")
+        res = {"valid": False}
+    return res
